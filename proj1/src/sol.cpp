4a093f7c1c1519c8eb14f8196ba391274b6156c1
Erik Leffler <er1110le-s@student.lu.se>
2020-08-25 17:39:22 +0000

    Finished project 1 (maybe)

--- proj1/src/sol.cpp
+++ proj1/src/sol.cpp
@@ -6,32 +6,36 @@
 
 void setup()
 {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+	// initialize LED digital pin as an output.
+	pinMode(LED_BUILTIN, OUTPUT);
+	Serial.begin(9600);
+
+
 }
 
 void busy_wait(int iter) {
-	double c = 2;
+	int8_t c = 2;
 	for(int i = 0; i < iter; i++) {
 		for(int j = 0; j < iter; j++) {
-			for(int k = 0; k < iter; k++) {
-				c /= (i + j + k);
-			}
+			c *= 2;
 		}
 	}
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+	int sensorValue = analogRead(A0);
+	Serial.println(sensorValue);
 
-  // wait for a second
-  busy_wait(1000);
+	// turn the LED on (HIGH is the voltage level)
+	digitalWrite(LED_BUILTIN, HIGH);
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
+	// wait for a second
+	busy_wait(sensorValue * 2);
 
-   // wait for a second
-  busy_wait(1000);
+	// turn the LED off by making the voltage LOW
+	digitalWrite(LED_BUILTIN, LOW);
+
+	// wait for a second
+	busy_wait(sensorValue * 2);
 }

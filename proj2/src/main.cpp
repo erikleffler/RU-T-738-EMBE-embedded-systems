2d9058ed2604221c78ab9e660efbb111b196f55a
Erik Leffler <er1110le-s@student.lu.se>
2020-09-11 17:22:47 +0000

    m

--- proj2/src/main.cpp
+++ proj2/src/main.cpp
@@ -1,46 +1,49 @@
 #include <Arduino.h>
-#include "../../arduino_lib/include/timer.h"
+#include "../../arduino_lib/include/button_d2.h"
 #include "../../arduino_lib/include/analog_out.h"
 #include "../../arduino_lib/include/analog_in.h"
-/*
-#include "../../arudino_lib/button.h"
 
-Button button;
-*/
-Timer timer;
+ButtonD2 button;
 AnalogOut led;
-AnalogIn photocell;
+AnalogIn input;
 
 void setup() {
 
 	Serial.begin(9600);
 	// Timer setup
-	timer.init(2);
 
    	// LED setup
-	led.init(3);
+	led.init(5);
 
 	// Photocell setup
-	photocell.init(A0);
-/*
-
+	input.init(A0);
 
 	// Button setup
-	button = Button(12);
-	button.init()
-	button.set_intterupt_func(
-		[&timer, &led] (void) {
-			timer.set_interrupt_func(led.toggle());
-		}
-	);*/
-}
+	button.init();
 
-/*
-ISR(TIMER1_COMPA_vect) {
-	led.toggle();
-}*/
+}
 
 void loop() {
-	led.set_val(255 - photocell.read());
-	Serial.println(photocell.read());
+	led.set_val(255 - input.read());
+	Serial.println(input.read());
 }
+
+
+// Here the object oriented stuff kinda breaks down a bit.
+// It's really messy to pass ISR callbacks on init so this has 
+// to be here. Got problems with obj member funcs as function pointers.
+// Could've worked with std::bind but arduino doesn't support STL.
+
+ISR(TIMER1_COMPA_vect) {
+	led.on_callback();
+}
+
+ISR(TIMER1_COMPB_vect) {
+	led.off_callback();
+}
+
+ISR(PCINT2_vect)
+{
+	led.disco = !led.disco;
+}
+
